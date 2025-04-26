5a19e68effe0ee7ecad4d05379a39de7980539f2
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2025-04-25 23:58:16 -0700

    setup spring-boot server

---
+++ server/src/main/java/prabhjot/adam/internship/prep/PrepApplication.java
@@ -0,0 +1,13 @@
+package prabhjot.adam.internship.prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepApplication.class, args);
+	}
+
+}
